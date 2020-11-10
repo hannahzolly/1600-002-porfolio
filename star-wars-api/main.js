61fd102628bc959ee334d337485643e008f113ed
hannah zollinger <hannahnicolezollinger@gmail.com>
2020-11-10 14:59:28 -0700

    Looped through films and people

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -1,2 +1,21 @@
 import { films } from './data/films.js'
-console.log(films[0])
+import { people } from './data/people.js'
+
+//console.log(document.body.children)
+
+console.log(people.length)
+
+const main = document.querySelector('main')
+
+/* films.forEach(film => {
+    console.log(film.title)
+    let newTitle = document.body.appendChild(document.createElement("h1"))
+    newTitle.textContent = film.title
+}) */
+
+people.forEach(person => {
+    console.log(person.name)
+    let personImg = document.createElement ('img')
+    personImg.src = "https://starwars-visualguide.com/assets/img/characters/10.jpg"
+    main.appendChild(personImg)
+})
