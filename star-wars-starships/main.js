2c90935512b15e78e0d663fd102db05f8b643d30
hannah zollinger <hannahnicolezollinger@gmail.com>
2020-11-10 23:51:41 -0700

    Starships

--- star-wars-starships/main.js
+++ star-wars-starships/main.js
@@ -6,7 +6,18 @@ const shipView = document.querySelector('.main')
 
 function populateNav(starships) {
     startships.forEach(starship => {
-        console.log(starship)
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        anchorWrap.addEventListener('click', event => {
+            console.log(event)
+        }
+
+        let listItem = document.createElement('li')
+        listItem.textContent = starship.name
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+        nav.appendChild(navList)
     })
 }
 
