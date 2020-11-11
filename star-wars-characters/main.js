f1890d36aaaf0a9dd2f2ae5b698b8573c4eaf823
hannah zollinger <hannahnicolezollinger@gmail.com>
2020-11-10 22:18:03 -0700

    Male and Female buttons

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -1,8 +1,10 @@
 import { people } from '../data/people.js'
 
+const mainContent = document.querySelector('main')
+
 const mainHeader = document.createElement('header')
 mainHeader.className = 'mainHeader'
-document.body.appendChild(mainHeader)
+document.body.insertBefore(mainHeader, mainContent)
 
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Male Characters'
@@ -16,3 +18,50 @@ const otherButton = document.createElement('button')
 otherButton.textContent = 'Other Characters'
 mainHeader.appendChild(otherButton)
 
+const maleCharacters = people.filter(person => person.gender === "male")
+console.log(maleCharacters)
+
+const femaleCharacters = people.filter(person => person.gender === 'female')
+
+
+maleButton.addEventListener('click', (event) => {
+  populateDOM(maleCharacters)
+
+})
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach(element => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement ('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error',() => charImg.hidden = true)
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+    })
+}
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start)=== '/') {
+        start++
+    
+    }
+    return url.slice(start, end)
+}
+
+function removeChildren(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
