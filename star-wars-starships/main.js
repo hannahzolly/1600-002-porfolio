e473eee99d02664eda1b81f667d1030c9efda939
hannah zollinger <hannahnicolezollinger@gmail.com>
2020-11-10 23:23:22 -0700

    Character display all, added starships files

---
+++ star-wars-starships/main.js
@@ -0,0 +1,13 @@
+import {starships } from '../data/starships.js'
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.main')
+
+function populateNav(starships) {
+    startships.forEach(starship => {
+        console.log(starship)
+    })
+}
+
+opulateNav(starships)
