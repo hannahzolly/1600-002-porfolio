4f087f0e571686c89a22d53855bc4f29e6280991
hannah zollinger <hannahnicolezollinger@gmail.com>
2020-12-10 12:26:09 -0700

    correct film name with film

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -7,8 +7,10 @@ for (let i = 0; i < 7; i++) {
     let figImg = document.createElement ('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCaption = document.createElement('figcaption')
-    figCaption.textContent = films[i].title
-    
+
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+
+    figCaption.textContent = foundFilm.title
     
     
     figure.appendChild(figImg)
